457232598a9aa9bc25b0219f4680222e5239366d
Sergey Ponomarev <stokito@gmail.com>
2024-10-19 11:35:32 +0300

    ExamplePlugin.java more comments

--- src/main/java/org/igniterealtime/openfire/exampleplugin/ExamplePlugin.java
+++ src/main/java/org/igniterealtime/openfire/exampleplugin/ExamplePlugin.java
@@ -5,12 +5,16 @@ import org.jivesoftware.openfire.container.PluginManager;
 
 import java.io.File;
 
+/**
+ * A sample plugin for Openfire.
+ */
 public class ExamplePlugin implements Plugin {
-    public void initializePlugin(PluginManager pluginManager, File file) {
 
+    public void initializePlugin(PluginManager pluginManager, File pluginDirectory) {
+        // Your code goes here
     }
 
     public void destroyPlugin() {
-
+        // Your code goes here
     }
 }
